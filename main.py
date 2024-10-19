2db77e89e7986828c8b89de450e672e29fd19fa7
Allonch <126762725+Allonch@users.noreply.github.com>
2024-10-20 00:34:04 +0200

    historic flights added
    I have edited extraction so it also gets historic data from an aircraft based on its icao. Moreover, I have created the dashboard function to create the final dashboard and added some lines to the main in order to ask for inputs.

---
+++ main.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+from src.dashboard import flights_dashboard
+from src.extraction import date_to_timestamp, extract_historic_aircraft_data, extract_openflights_data, extract_live_flight_data
+from src.transformation import clean_airport_data, enrich_flight_data
+from src.loading import load_data_to_postgres
+from src.utils import log_message
+
+
+def run_etl():
+    # Input
+    entry = input("\n\n\nIntroduce the aircraft's ICAO (hexadecimal code) and the start date (format: ICAO;YYYY-MM-DD hh:mm:ss):\n IMPORTANT: To comply with OpenSky access policies, the start date must not be more than 7 DAYS from the current time!!: ")
+    parts = entry.split(';')
+
+    def validate_date(date_str):
+        try:
+            date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+            return date
+        except ValueError:
+            print("Start date format not valid. Must be YYYY-MM-DD hh:mm:ss")
+            return None
+
+    # Verify parts
+    if len(parts) != 2:
+        print("Error: Must introduce ICAO and start date in the correct format and separated by ;")
+    else:
+        icao = parts[0].strip() 
+        begin = validate_date(parts[1].strip()).strftime("%Y-%m-%d %H:%M:%S")
+        end = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # current time
+
+        # Check dates
+        if begin is not None and end is not None:
+            if end < begin:
+                print("Error: Start date cannot be in the future.")
+            if abs(datetime.strptime(end, "%Y-%m-%d %H:%M:%S")-datetime.strptime(begin, "%Y-%m-%d %H:%M:%S")) > timedelta(days=7):
+                print("Error: Start date is more than 7 days from the current time (now).")
+
+    # Extraction
+    log_message("Starting extraction phase.")
+    airports_df, planes_df = extract_openflights_data()
+    flights_df = extract_live_flight_data()
+    historic_aircraft_flights_df = extract_historic_aircraft_data(icao, date_to_timestamp(begin), date_to_timestamp(end))
+
+    # Transformation
+    log_message("Starting transformation phase.")
+    cleaned_airports_df = clean_airport_data(airports_df)
+    enriched_flights_df = enrich_flight_data(flights_df)
+
+    # Loading
+    log_message("Starting loading phase.")
+    load_data_to_postgres(cleaned_airports_df, "airports")
+    load_data_to_postgres(planes_df, "planes")
+    load_data_to_postgres(historic_aircraft_flights_df, "historic_flights_aircraft")
+    load_data_to_postgres(enriched_flights_df, "flights")
+
+    # Dashboard
+    log_message("Generating dashboard.")
+    flights_dashboard = flights_dashboard(icao)
+    load_data_to_postgres(flights_dashboard, f"flights_of_aircraft_{icao}")
+    print(flights_dashboard)
+
+
+if __name__ == "__main__":
+    run_etl()
---
+++ src/extraction.py
@@ -0,0 +1,72 @@
+import requests
+import pandas as pd
+from datetime import datetime
+import sys
+import os
+sys.path.append(os.path.abspath('./opensky_api/python'))
+from opensky_api import OpenSkyApi
+
+def extract_openflights_data():
+    # Airports
+    airports_url = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
+    airports_cols = ['airportID', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude',
+                     'timezone', 'dst', 'tzDatabaseTimezone', 'type', 'source']
+    airports_df = pd.read_csv(airports_url, header=None, names=airports_cols)
+
+    # Planes
+    planes_url = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/planes.dat"
+    planes_cols = ['name', 'iata', 'icao']
+    planes_df = pd.read_csv(planes_url, header=None, names=planes_cols)
+
+    return airports_df, planes_df
+
+def extract_live_flight_data():
+    opensky_url = "https://opensky-network.org/api/states/all"
+    response = requests.get(opensky_url)
+    if response.status_code == 200:
+        columns = ['icao24', 'callsign', 'origin_country', 'time_position', 'last_contact', 'longitude', 'latitude',
+                   'baro_altitude', 'on_ground', 'velocity', 'true_track', 'vertical_rate', 'sensors', 'geo_altitude',
+                   'squawk', 'spi', 'position_source']
+        flights_df = pd.DataFrame(response.json()['states'], columns=columns)
+        return flights_df
+    else:
+        raise Exception(f"Error fetching data: {response.status_code}")
+    
+# Converts a date to Unix date format
+def date_to_timestamp(date):
+    dt = datetime.strptime(date, "%Y-%m-%d %H:%M:%S") # example: "2024-10-19 12:00:00". Last 7 days max
+    timestamp = int(dt.timestamp())
+    return timestamp
+
+def timestamp_to_date(timestamp):
+    return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    
+def extract_historic_aircraft_data(icao, begin, end):
+    api = OpenSkyApi("allonch", "airlife2025")
+    raw_data = api.get_flights_by_aircraft(icao, begin, end)
+    data = [list(f.keys) for f in raw_data]
+    columns = ['icao24', 'first_seen', 'est_departure_airport', 'last_seen', 'est_arrival_airport', 
+                'callsign', 'est_departure_airport_horiz_distance', 'est_departure_airport_vert_distance', 
+                'est_arrival_airport_horiz_distance', 'est_arrival_airport_vert_distance', 
+                'departure_airport_candidates_count', 'arrival_airport_candidates_count']
+    historic_flights_df = pd.DataFrame(data, columns=columns)
+
+    # Convert last and first seen to date format
+    historic_flights_df['first_seen'] = pd.to_numeric(historic_flights_df['first_seen'], errors='coerce')
+    historic_flights_df['first_seen'] = pd.to_datetime(historic_flights_df['first_seen'], unit='s')
+    historic_flights_df['last_seen'] = pd.to_numeric(historic_flights_df['last_seen'], errors='coerce')
+    historic_flights_df['last_seen'] = pd.to_datetime(historic_flights_df['last_seen'], unit='s')
+
+    # If there is no estimated arrival airport, the flight might be still in the air
+    historic_flights_df['on_ground'] = historic_flights_df['est_arrival_airport'].notna()
+    
+    return historic_flights_df
+    
+
+if __name__ == "__main__":
+    begin = "2024-10-13 12:00:00"
+    end = "2024-10-19 12:00:00" # 7 days max time span to request from current time
+    # icao = "a2b47e" # This corresponds to a Delta Air Lines aircraft (a Boeing 737)
+    icao = "3c675a"
+    historic_aircraft_flights_df = extract_historic_aircraft_data(icao, date_to_timestamp(begin), date_to_timestamp(end))
+    # 3c675a;2024-10-13 12:00:00
